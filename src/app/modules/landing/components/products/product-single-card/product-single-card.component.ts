67136323179f2c981994fec3037dc04b2d2a7b45
Enes Esen <enesen@gmx.com>
2023-11-17 16:31:43 +0300

    fix: optimize pages loading

--- src/app/modules/landing/components/products/product-single-card/product-single-card.component.ts
+++ src/app/modules/landing/components/products/product-single-card/product-single-card.component.ts
@@ -1,13 +1,13 @@
-import { CurrencyPipe, NgStyle } from '@angular/common';
+import { CurrencyPipe, NgOptimizedImage, NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 import type { Product } from '@/modules/landing/models/product';
 
 @Component({
-	selector: '[product-single-card]',
+	selector: 'app-product-single-card',
 	templateUrl: './product-single-card.component.html',
 	standalone: true,
-	imports: [NgStyle, CurrencyPipe],
+	imports: [NgStyle, CurrencyPipe, NgOptimizedImage],
 })
 export class ProductSingleCardComponent {
 	@Input() product: Product = <Product>{};

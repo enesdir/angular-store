ec66dae7d21c2dfa4a98bce99068b34707a67d25
Enes Esen <enesen@gmx.com>
2023-11-20 21:59:21 +0300

    fix: ssr compatible directive

--- src/app/shared/directives/click-outside.directive.spec.ts
+++ src/app/shared/directives/click-outside.directive.spec.ts
@@ -1,8 +1,43 @@
+import { Component, DebugElement, PLATFORM_ID } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 import { ClickOutsideDirective } from './click-outside.directive';
 
+@Component({
+	template: ` <div clickOutside (clickOutside)="onClickedOutside($event)"></div> `,
+})
+class TestComponent {
+	// eslint-disable-next-line unused-imports/no-unused-vars, @typescript-eslint/no-unused-vars
+	onClickedOutside(event: Event) {}
+}
+
 describe('ClickOutsideDirective', () => {
+	let component: TestComponent;
+	let fixture: ComponentFixture<TestComponent>;
+	let divEl: DebugElement;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [ClickOutsideDirective, TestComponent],
+			providers: [{ provide: PLATFORM_ID, useValue: 'browser' }],
+		});
+
+		fixture = TestBed.createComponent(TestComponent);
+		component = fixture.componentInstance;
+		divEl = fixture.debugElement.query(By.css('div'));
+
+		fixture.detectChanges();
+	});
+
 	it('should create an instance', () => {
-		const directive = new ClickOutsideDirective();
+		const directive = new ClickOutsideDirective(divEl, document, TestBed.inject(PLATFORM_ID));
 		expect(directive).toBeTruthy();
+	});
+
+	it('should emit event when clicked outside', () => {
+		spyOn(component, 'onClickedOutside');
+		divEl.triggerEventHandler('click', null);
+		expect(component.onClickedOutside).toHaveBeenCalled();
 	});
 });
--- src/app/shared/directives/click-outside.directive.ts
+++ src/app/shared/directives/click-outside.directive.ts
@@ -1,8 +1,18 @@
-import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, Directive, ElementRef, EventEmitter, Inject, OnDestroy, Output } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import {
+	AfterViewInit,
+	Directive,
+	ElementRef,
+	EventEmitter,
+	Inject,
+	OnDestroy,
+	Output,
+	PLATFORM_ID,
+} from '@angular/core';
 import { filter, fromEvent, Subscription } from 'rxjs';
 
 @Directive({
+	// eslint-disable-next-line @angular-eslint/directive-selector
 	selector: '[clickOutside]',
 	standalone: true,
 })
@@ -10,22 +20,24 @@ export class ClickOutsideDirective implements AfterViewInit, OnDestroy {
 	@Output() clickOutside = new EventEmitter<void>();
 
 	documentClickSubscription: Subscription | undefined;
-
 	constructor(
 		private element: ElementRef,
-		@Inject(DOCUMENT) private document: Document
+		@Inject(DOCUMENT) private document: Document,
+		@Inject(PLATFORM_ID) private platformId: object
 	) {}
 
 	ngAfterViewInit(): void {
-		this.documentClickSubscription = fromEvent(this.document, 'click')
-			.pipe(
-				filter((event) => {
-					return !this.isInside(event.target as HTMLElement);
-				})
-			)
-			.subscribe(() => {
-				this.clickOutside.emit();
-			});
+		if (isPlatformBrowser(this.platformId)) {
+			this.documentClickSubscription = fromEvent(this.document, 'click')
+				.pipe(
+					filter((event) => {
+						return !this.isInside(event.target as HTMLElement);
+					})
+				)
+				.subscribe(() => {
+					this.clickOutside.emit();
+				});
+		}
 	}
 
 	ngOnDestroy(): void {

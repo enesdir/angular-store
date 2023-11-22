d56d54969b8053e1d56ee8c68475adc7587c91cc
Enes Esen <enesen@gmx.com>
2023-11-23 00:54:59 +0300

    fix: update chunks

--- src/app/shared/components/logo/logo.component.ts
+++ src/app/shared/components/logo/logo.component.ts
@@ -1,10 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-logo',
 	templateUrl: './logo.component.html',
 	standalone: true,
+	imports: [NgOptimizedImage],
 })
 export class LogoComponent {
 	constructor() {}
+	public logoUrl = '/assets/icons/logo.png';
 }
